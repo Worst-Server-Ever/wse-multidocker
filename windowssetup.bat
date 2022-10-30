53bc6fd47f9c017e013d45cfb727250667d694eb
“insert-usernamehere“ <insert@insert.tk>
2022-10-30 17:12:51 -0400

    more changes because I can't sleep unless its perfect

--- windowssetup.bat
+++ windowssetup.bat
@@ -1,5 +1,7 @@
 @ECHO OFF
+mkdir data
 echo Hello there, this script is intented to help you setup your computer ot contribute to the multipaper worstserverever node, allowing us to keep the server alive!
+echo if you've run this script before and haven't replaced any files you can spam enter without missing anything
 pause
 echo Ok lets start things off, what would you like this server to be named (please use a name that is orriginal and easily identifiable to avoid causing issues)
 set /p node-name=name:
@@ -14,8 +16,10 @@ set /p vd=Viewdistance (max 16):
 fart.exe docker-compose.yml cmdargram %ramuse%G -q
 fart.exe docker-compose.yml cmdargvd %vd% -q
 fart.exe docker-compose.yml cmdargnodename %node-name% -q
-powershell -Command "Invoke-WebRequest https://nextcloud.insert.tk/s/cCZzbwXb5amrecS/download -OutFile serverfiles.zip
-powershell Expand-Archive serverfiles.zip -DestinationPath data"
+rd /s /q data
+powershell -Command "Invoke-WebRequest https://nextcloud.insert.tk/s/cCZzbwXb5amrecS/download -OutFile serverfiles.zip"
+powershell -Command "Expand-Archive serverfiles.zip -DestinationPath data"
+del serverfiles.zip
 echo you will now be connected to the master server, open a support ticket on discord to add your machine to the server list
 pause
 docker-compose pull
